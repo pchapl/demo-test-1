8592801c714ce139db3a81bcdfef68bc67891a27
Pavel Chaplygin <mail@pchapl.dev>
2023-02-07 15:08:56 +0200

    Update header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  document.body.insertAdjacentHTML('afterbegin', '<h1>webpacked</h1>')
+  document.body.insertAdjacentHTML('afterbegin', '<h1>Demo project</h1>')
 });
